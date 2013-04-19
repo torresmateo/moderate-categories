87498acca32e79c8cd4df33681ead4e18b0d23b3
Maluchi <torresmateo@gmail.com>
2013-04-19 14:35:02 -0400

    home test 2

---
+++ views/mainMenu.php
@@ -0,0 +1,13 @@
+<!-- Main Menu Template
+
+Shows the role-category rules table and the "Add Rule" button
+
+Mateo Torres 2013-->
+
+<?php
+//include the header file
+include(plugin_dir_url(__FILE__)."/template/header.php");
+//include the footer file
+include(plugin_dir_url(__FILE__)."/template/footer.php");
+?>
+
