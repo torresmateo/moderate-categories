7d301a26f70d514677e8b3c7250caef0df8a4697
Maluchi <torresmateo@gmail.com>
2013-04-22 07:24:28 -0400

    Added the table placeholder to mainMenu

--- views/mainMenu.php
+++ views/mainMenu.php
@@ -8,7 +8,36 @@ Mateo Torres 2013-->
 //include the header file
 include("template/header.php");?>
 
-
+<form action="<?php echo $PHP_SELF;?>" method="post" enctype="multipart/form-data" name="update_roles_rules">
+    <h2>Category rules for Roles</h2>
+    <table class="widefat post" cellspacing="0">
+        <thead>
+            <tr>
+                <th scope="col" class="manage-column">Role</th>
+                <th scope="col" class="manage-column">Categories</th>
+                <th scope="col" class="manage-column">Add</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>asdf</td>
+                <td>
+                    <ul>
+                        <li>asdf</li>
+                        <li>asdf</li>
+                        <li>asdf</li>
+                        <li>asdf</li>
+                        <li>asdf</li>
+                        <li>asdf</li>
+                    </ul>
+                </td>
+                <td>
+                    EDITAR
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</form>
 
 <?php //include the footer file
 include("template/footer.php");?>
